e7e0c195011b91fc9f5ff616e97517324814f371
frodulec <jakubkrasniewski@gmail.com>
2021-09-13 22:37:21 +0200

    update

--- scan.py
+++ scan.py
@@ -1,28 +1,49 @@
 import win32api
 import os
 from os.path import isfile, join, isdir
+from shutil import copyfile
 
-drives = win32api.GetLogicalDriveStrings()
-drives = drives.split('\000')[:-1]
-cur_dir = os.path.abspath(os.getcwd())
 
-def process_dir(dir):
-    for element in os.listdir(dir):
-        joined = join(dir, element)
+def read_extensions():
+    with open('extensions', 'rb') as ext_file:
+        line = ext_file.readline()
+        extensions = line.decode().split(sep=',')
+        print(extensions)
+        return extensions
+
+
+def process_dir(directory, extensions):
+    for element in os.listdir(directory):
+        joined = join(directory, element)
         if isfile(joined):
-            if element.endswith('.xlsx') or element.endswith('.pdf') or element.endswith('.xls'):
+            print(element)
+
+            if any(extension in element for extension in extensions):
+                # print(joined)
+                # command = 'copy ' + joined + ' .\\found\\'
+                copyfile(joined, '.\\found\\')
                 print(joined)
-                command = 'copy ' + joined + ' ' + cur_dir + '\\'
-                os.system(command)
+            if element.endswith("pdf"):
+                print(element)
+
+
         elif isdir(joined):
             try:
-                process_dir(joined)
+                process_dir(joined, extensions)
             except Exception:
                 pass
 
 
+drives = win32api.GetLogicalDriveStrings()
+drives = drives.split('\000')[:-1]
+os.system('mkdir found')
+cur_dir = os.path.abspath(os.getcwd())
+cur_dir += '\\found'
+
+exts = read_extensions()
+
 for drive in enumerate(drives):
     try:
-        process_dir(drive[1])
+        process_dir(drive[1], exts)
     except Exception:
         pass
